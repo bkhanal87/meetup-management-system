792aa28acf5a20896b9a259420c5c568b7dcb5da
bkhanal87@gmail.com <bkhanal87@gmail.com>
2025-01-18 07:23:52 -0500

    basic navbar using material ui. Need to incorporate routing later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent,
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
